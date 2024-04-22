6386201e284bea8da36b1000ceba0f3c3d90999a
Devansh <devanshsrivastava707@gmail.com>
2024-04-22 16:02:43 +0530

    Setting Up Basic configurations of VSCode and Extensions  for live Server
    Setting Up Basic configurations of VSCode and Extensions  for live Server

---
+++ 03-Developer-Skills/starter/script.js
@@ -0,0 +1,14 @@
+// Remember, we're gonna use strict mode in all scripts now!
+"use strict";
+
+const x = 23;
+
+if (x === 23) {
+  console.log(23);
+}
+
+const calcAge = (birthYear) => 2020 - birthYear;
+
+console.log(calcAge(2000));
+console.log(calcAge(1999));
+console.log(calcAge(1990));
