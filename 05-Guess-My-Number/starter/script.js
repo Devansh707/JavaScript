8134846bf72baea3aa9176364f01c945434a0f3f
Devansh <devanshsrivastava707@gmail.com>
2024-04-23 12:28:39 +0530

    Implementing Game logic
    Implementing Game logic

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -14,13 +14,36 @@ document.querySelector('.guess').value = 25;
 console.log(document.querySelector('.guess').value);
 */
 
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
+
+let score = 20;
+
 document.querySelector('.check').addEventListener('click', function () {
   //   console.log(document.querySelector('.guess').value);
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
 
+  if (score <= 0) {
+    document.querySelector('.message').textContent = 'You Lost the Game !!!';
+    return;
+  }
+
   if (!guess) {
     document.querySelector('.message').textContent =
       'No Number! Please Enter a Number to check the guess.';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent =
+      'Wohoo !! Correct Number!!';
+  } else if (guess > secretNumber) {
+    document.querySelector('.message').textContent =
+      'Too High Guess!! Please Lower your guess!!';
+    score--;
+    document.querySelector('.score').textContent = score;
+  } else if (guess < secretNumber) {
+    document.querySelector('.message').textContent =
+      'Too Low Guess!! Please increase your guess!!';
+    score--;
+    document.querySelector('.score').textContent = score;
   }
 });
