f45f3a4ad8db3a834ec23ec4865889ea953c8bf3
Devansh <devanshsrivastava707@gmail.com>
2024-04-24 12:43:22 +0530

    Project , Dom Manipulation with CSS and handling key events Key
    Dom Manipulation with CSS and handling key events Key  and completion of Show Modal Project.

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -6,6 +6,36 @@ const btnCLoseModal = document.querySelector(".close-modal");
 const btnsOpenModal = document.querySelectorAll(".show-modal");
 console.log(btnsOpenModal);
 
+const openModal = function () {
+  console.log("Button Clicked");
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
 for (let i = 0; i < btnsOpenModal.length; i++) {
   console.log(btnsOpenModal[i].textContent);
 }
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener("click", openModal);
+}
+
+btnCLoseModal.addEventListener("click", closeModal); // we are not calling closeModal() as JS will immediately call the function as soon as the JS executes the code is rendered but we want this function to be executed only when button is click so we only specify closeModal in arguements.
+
+overlay.addEventListener("click", closeModal);
+
+// ------ Keyboard Events ---//
+//  Keyboard Events are global events as they will not operate on 1 specific event and it will listen/operate on whole document/page.
+
+document.addEventListener("keydown", function (e) {
+  console.log(e.key);
+
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+});
